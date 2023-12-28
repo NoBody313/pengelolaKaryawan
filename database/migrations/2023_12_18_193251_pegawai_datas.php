f992b38e61047c831b1f398e18fcffe7c224b7a4
NoBody313 <muhammadfatihmaulana434@gmail.com>
2023-12-29 04:30:22 +0700

    push

--- database/migrations/2023_12_18_193251_pegawai_datas.php
+++ database/migrations/2023_12_18_193251_pegawai_datas.php
@@ -14,46 +14,41 @@ return new class extends Migration
         Schema::create('pegawai_datas', function (Blueprint $table) {
             $table->id();
             $table->integer('urutan')->nullable();
-            $table->integer('nik_admedika');
-            $table->integer('nik_tg');
-            $table->string('nama');
-            $table->string('jenis_kelamin');
-            $table->string('agama');
-            $table->string('kota_lahir');
-            $table->date('tanggal_lahir');
-            $table->string('tahun_lahir');
-            $table->bigInteger('no_ktp');
-            // KTP
-            $table->text('alamat_ktp');
-            $table->text('provinsi_ktp');
-            $table->text('kab_kota_ktp');
-            $table->text('kec_ktp');
-            $table->text('kel_ktp');
-            $table->string('kodepos_ktp');
-            // Domisili
-            $table->text('alamat_domisili');
-            $table->text('provinsi_domisili');
-            $table->text('kab_kota_domisili');
-            $table->text('kec_domisili');
-            $table->text('kel_domisili');
-            $table->string('kodepos_domisili');
+            $table->integer('nik_admedika')->nullable();
+            $table->integer('nik_tg')->nullable();
+            $table->string('nama')->nullable();
+            $table->string('jenis_kelamin')->nullable();
+            $table->string('agama')->nullable();
+            $table->string('kota_lahir')->nullable();
+            $table->date('tanggal_lahir')->nullable();
+            $table->string('tahun_lahir')->nullable();
+            $table->bigInteger('no_ktp')->nullable();
 
-            $table->string('status_pernikahan');
-            $table->date('tanggal_pernikahan');
-            $table->integer('jumlah_anak');
-            $table->string('pendidikan_terakhir');
-            $table->text('jurusan_pendidikan_terakhir');
-            $table->text('nama_institusi');
-            $table->text('kota_institusi');
-            $table->year('lulus_thn_pendidikan_terakhir');
+            $table->text('alamat_ktp')->nullable();
+            $table->text('kab_kota_ktp')->nullable();
+            $table->text('provinsi_ktp')->nullable();
+            $table->string('kodepos_ktp')->nullable();
+            $table->text('alamat_domisili')->nullable();
+            $table->text('kab_kota_domisili')->nullable();
+            $table->text('provinsi_domisili')->nullable();
+            $table->string('kodepos_domisili')->nullable();
 
-            $table->char('no_hp_tsel');
-            $table->char('no_hp_nontsel');
-            $table->text('nama_kontak_emergency');
-            $table->char('no_hp_emergency');
-            $table->text('hubungan_kontak_emergency');
-            $table->string('email_pribadi');
-            $table->text('nama_ibu');
+            $table->string('status_pernikahan')->nullable();
+            $table->date('tanggal_pernikahan')->nullable();
+            $table->integer('jumlah_anak')->nullable();
+            $table->string('pendidikan_terakhir')->nullable();
+            $table->text('jurusan_pendidikan_terakhir')->nullable();
+            $table->text('nama_institusi')->nullable();
+            $table->text('kota_institusi')->nullable();
+            $table->year('lulus_thn_pendidikan_terakhir')->nullable();
+
+            $table->string('email_pribadi')->nullable();
+            $table->char('no_hp_tsel')->nullable();
+            $table->char('no_hp_nontsel')->nullable();
+            $table->char('no_hp_emergency')->nullable();
+            $table->text('nama_kontak_emergency')->nullable();
+            $table->text('hubungan_kontak_emergency')->nullable();
+            $table->text('nama_ibu')->nullable();
 
             $table->timestamps();
         });
