6cdf6327a2823245aeeae9f41dfa07fcc6a2a131
NoBody313 <muhammadfatihmaulana434@gmail.com>
2024-02-01 10:05:15 +0700

    Security Part 2 : Header 🚀

--- resources/views/helper/helper.blade.php
+++ resources/views/helper/helper.blade.php
@@ -2,6 +2,7 @@
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@200;300;400;500;600&display=swap"
     rel="stylesheet">
+<link rel="shortcut icon" href="{{ asset('images/admedLogo.png') }}" type="image/x-icon">
 
 {{-- Select2 --}}
 <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
